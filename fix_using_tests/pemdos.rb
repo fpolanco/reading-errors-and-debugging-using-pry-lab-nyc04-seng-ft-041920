1ce48076d7e86839803368343d75ff839fbf214f
Franchell Polanco <polanco.franchell@gmail.com>
2020-04-23 14:56:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,12 @@
-# don't forget to add: require 'pry'
+require 'pry'# don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
+
+  if "string"[0] == "s"
+    "s" * 10 + string
+    #10.times do "s" + "string"
+      #binding.pry
+  #else
+  #  "string"
   end
 end
